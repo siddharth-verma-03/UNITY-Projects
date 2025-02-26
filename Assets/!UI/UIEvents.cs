e3b945359f20dd085d589a72b34708b736ca6058
Siddharth Verma <siddharthverma424@gmail.com>
2025-02-27 02:09:04 +0530

    UI Change

---
+++ Assets/!UI/UIEvents.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIEvents : MonoBehaviour
+{
+    
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
+   
+}
